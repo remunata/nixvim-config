9bd1844f694862744f068e66d50fe0a92b7e830c
Mahendra Dinata <remdinata@gmail.com>
2025-10-06 13:56:35 +0700

    add few configs to blink-cmp

--- config/plugins/cmp/blink-cmp.nix
+++ config/plugins/cmp/blink-cmp.nix
@@ -1,5 +1,63 @@
+{ pkgs, ... }:
 {
   plugins.blink-cmp = {
     enable = true;
+
+    settings = {
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = false;
+          };
+        };
+
+        documentation = {
+          auto_show = true;
+          auto_show_delay_ms = 200;
+
+          window = {
+            border = "rounded";
+          };
+        };
+
+        ghost_text = {
+          enabled = true;
+        };
+
+        list = {
+          selection = {
+            auto_insert = true;
+            preselect = true;
+          };
+        };
+
+        trigger = {
+          show_on_accept_on_trigger_character = true;
+          show_on_keyword = true;
+          show_on_trigger_character = true;
+        };
+      };
+
+      fuzzy = {
+        implementation = "rust";
+
+        prebuilt_binaries = {
+          download = false;
+          force_version = "v${pkgs.vimPlugins.blink-cmp.version}";
+        };
+      };
+
+      snippets = {
+        preset = "luasnip";
+      };
+    };
+
+    signature = {
+      enabled = true;
+
+      window = {
+        border = "rounded";
+      };
+    };
   };
 }
